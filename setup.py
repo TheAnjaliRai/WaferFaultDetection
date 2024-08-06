6dbc8103585c00e0dd0c073617b82133e0e833bf
TheAnjaliRai <anjalirai0004@gmail.com>
2024-08-06 14:53:14 +0530

    Fix Cython and PyYAML build issues

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     author="anjali",
     author_email="an.anjalirai98@gmail.com",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['Cython>=0.29.30',],
+    setup_requires=['Cython>=0.29.30',  # Required for building extensions
+    ],
 )
